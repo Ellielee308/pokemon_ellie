9342ea925048634ded5c93f35a0406f2f3c216a4
Ellie Lee <as152808@gmail.com>
2024-12-06 20:22:51 +0800

    feat: add go-back icon

--- src/assets/icon.jsx
+++ src/assets/icon.jsx
@@ -31,3 +31,20 @@ export const ArrowNext = () => (
     />
   </svg>
 );
+
+export const ArrowGoBack = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={3}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M15.75 19.5 8.25 12l7.5-7.5"
+    />
+  </svg>
+);
