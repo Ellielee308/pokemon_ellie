c216239d624b97ac9b8fa25dff993e42f618eb3c
Ellie Lee <as152808@gmail.com>
2024-12-06 17:35:50 +0800

    feat: RWD for digimon list

--- src/pages/DigimonList/index.jsx
+++ src/pages/DigimonList/index.jsx
@@ -52,21 +52,27 @@ function DigimonList() {
   };
 
   return (
-    <>
-      <div className="flex h-14 w-screen justify-center bg-red-600 px-12">
-        <div className="flex w-[880px] items-center justify-between">
+    <div className="w-screen">
+      <div className="flex h-14 justify-center bg-red-600 px-2 sm:px-12">
+        <div className="flex w-full items-center justify-between md:w-[880px]">
           <h1 className="font-oxanium text-xl font-bold text-white">
             Digimon Encyclopedia
           </h1>
           <Link
             to="pokemon?page=1"
-            className="font-oxanium h-6 rounded-2xl bg-white px-4 text-sm font-semibold leading-6 hover:bg-slate-100"
+            className="font-oxanium hidden h-6 rounded-2xl bg-white px-4 text-sm font-semibold leading-6 hover:bg-slate-100 md:block"
           >
             Go to Pokémon Encyclopedia
           </Link>
+          <Link
+            to="pokemon?page=1"
+            className="font-oxanium h-6 rounded-2xl bg-white px-2 text-xs font-semibold leading-6 hover:bg-slate-100 md:hidden"
+          >
+            Pokémon ➜
+          </Link>
         </div>
       </div>
-      <div className="flex h-fit min-h-[calc(100vh-56px)] flex-col items-center justify-between bg-zinc-800 py-8">
+      <div className="flex h-fit min-h-[calc(100vh-56px)] w-full flex-col items-center justify-between bg-zinc-800 py-3 md:py-8">
         {digimonItems.length === 0 ? (
           <div className="font-oxanium flex flex-grow flex-col items-center justify-center text-white">
             <p className="mb-4 text-3xl">Oops! No Digimons available.</p>
@@ -81,17 +87,17 @@ function DigimonList() {
             </button>
           </div>
         ) : (
-          <div className="mb-12 grid w-[880px] grid-cols-2 gap-8 px-12">
+          <div className="mb-12 grid grid-cols-2 gap-2 px-2 sm:gap-8 sm:px-12 md:w-[880px]">
             {digimonItems.map((item) => (
               <div
                 key={item.id}
-                className="flex h-80 w-full flex-col items-center justify-start rounded-lg bg-white px-2 py-8"
+                className="flex flex-col items-center justify-start rounded-lg bg-white px-2 py-4 md:h-80 md:w-full md:py-8"
               >
                 <img
                   src={item.image}
                   className="aspect-square w-1/2 object-contain"
                 />
-                <h3 className="font-oxanium mt-8 text-center text-xl font-bold">
+                <h3 className="font-oxanium mt-8 text-center text-base font-bold sm:text-lg md:text-xl">
                   {item.name}
                 </h3>
               </div>
@@ -127,7 +133,7 @@ function DigimonList() {
           </button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
